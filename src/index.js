195c7519441ae400d6a78599f000084dc4376750
h-genie <ses2000hj@hanmail.net>
2020-09-24 15:24:46 +0900

    2.0

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import fbase from './fbase';
+
+ReactDOM.render(<App />, document.getElementById('root'));
