5fe7bdace65c6d9b5f140ca18f6d3b02d9b2ef35
H-Genie <genie@seohyeongjin-ui-MacBookAir.local>
2020-10-10 01:52:12 +0900

    Updates

---
+++ precache-manifest.55baaf5d8c6127752f7248e931243ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b15c15ae6569b881baeec6f2fa7ed1",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "fb155591e69dbb4baad5",
+    "url": "/nwitter/static/css/main.86fde8f1.chunk.css"
+  },
+  {
+    "revision": "5c5da9a81a4e4eaaec57",
+    "url": "/nwitter/static/js/2.aa3d16d8.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter/static/js/2.aa3d16d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb155591e69dbb4baad5",
+    "url": "/nwitter/static/js/main.70d23da5.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
