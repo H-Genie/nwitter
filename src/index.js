052d3eb651916e812e5c51eef7a1426777aa1253
h-genie <ses2000hj@hanmail.net>
2020-10-10 01:33:34 +0900

    6.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "components/App";
+import "./styles.css";
 
-import App from './components/App';
-import fbase from './fbase';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
