8e42c0202474164147907765f8b99957fdcaafbe
h-genie <ses2000hj@hanmail.net>
2021-06-13 19:57:06 +0900

    Updates

---
+++ precache-manifest.ac8eace7ffd04e4ed66979c88da208d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70a67aad3e8e4948b9da8b5be001c79",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "602b6bf6ad042ffc392a",
+    "url": "/nwitter/static/css/main.6d486925.chunk.css"
+  },
+  {
+    "revision": "e880997286d6e039b318",
+    "url": "/nwitter/static/js/2.4b0839d8.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter/static/js/2.4b0839d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602b6bf6ad042ffc392a",
+    "url": "/nwitter/static/js/main.4f82c4ee.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nwitter/precache-manifest.220de9a916ed365ea90e08ebafe5dd69.js"
+  "/nwitter/precache-manifest.ac8eace7ffd04e4ed66979c88da208d6.js"
 );
 
 self.addEventListener('message', (event) => {
